87b7462132bf8e5962533ec119e3f339f64f1f75
Edgars Mjasnikovs <mjasnikovs@gmail.com>
2017-06-05 21:42:04 +0300

    Gulpfile update

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 const gulp = require('gulp')
 const connect = require('gulp-connect')
+const htmlmin = require('gulp-htmlmin')
+const cleanCSS = require('gulp-clean-css')
 
 gulp.task('connect', () => {
 	connect.server({
@@ -15,6 +17,29 @@ gulp.task('html', () => {
 
 gulp.task('watch', () => {
 	gulp.watch(['./src/**'], ['html'])
+})
+
+gulp.task('minify-html', () => {
+	return gulp.src('./src/*.html')
+		.pipe(htmlmin({collapseWhitespace: true}))
+		.pipe(gulp.dest('dist'))
+})
+
+gulp.task('minify-css', () => {
+	return gulp.src('./src/css/*')
+		.pipe(cleanCSS({compatibility: 'ie8'}))
+		.pipe(gulp.dest('dist/css'))
+})
+
+const filesToCopy = [
+	'./src/.htaccess',
+	'./src/favicon.ico',
+	'./src/robots.txt'
+]
+
+gulp.task('move-files', () => {
+	return gulp.src(filesToMove, {base: './'})
+		.pipe(gulp.dest('./public/'))
 })
 
 gulp.task('server', ['connect', 'watch'])
