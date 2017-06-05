e70ab81843d633d31704605db0dd3dfdc8f63a4c
Edgars Mjasnikovs <mjasnikovs@gmail.com>
2017-06-05 22:03:46 +0300

    Gulpfile update

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ const gulp = require('gulp')
 const connect = require('gulp-connect')
 const htmlmin = require('gulp-htmlmin')
 const cleanCSS = require('gulp-clean-css')
+const imagemin = require('gulp-imagemin')
 
 gulp.task('connect', () => {
 	connect.server({
@@ -22,27 +23,40 @@ gulp.task('watch', () => {
 gulp.task('minify-html', () => {
 	return gulp.src('./src/*.html')
 		.pipe(htmlmin({collapseWhitespace: true}))
-		.pipe(gulp.dest('dist'))
+		.pipe(gulp.dest('./dist'))
 })
 
 gulp.task('minify-css', () => {
 	return gulp.src('./src/css/*')
 		.pipe(cleanCSS({compatibility: 'ie8'}))
-		.pipe(gulp.dest('dist/css'))
+		.pipe(gulp.dest('./dist/css'))
 })
 
-const filesToCopy = [
-	'./src/.htaccess',
-	'./src/favicon.ico',
-	'./src/robots.txt'
-]
+gulp.task('minify-img', () => {
+	return gulp.src('src/images/*')
+		.pipe(imagemin())
+		.pipe(gulp.dest('./dist/images'))
+})
+
+gulp.task('move-js', () => {
+	return gulp.src([
+		'./src/js/bootstrap.min.js',
+		'./src/js/jquery-3.2.1.min.js',
+		'./src/js/main.js'
+	], {base: './src'})
+		.pipe(gulp.dest('./dist'))
+})
 
 gulp.task('move-files', () => {
-	return gulp.src(filesToCopy, {base: './'})
-		.pipe(gulp.dest('./public/'))
+	return gulp.src([
+		'./src/.htaccess',
+		'./src/favicon.ico',
+		'./src/robots.txt'
+	], {base: './src'})
+		.pipe(gulp.dest('./dist/'))
 })
 
-gulp.task('build', ['move-files', 'minify-html', 'minify-css'])
+gulp.task('build', ['move-files', 'minify-html', 'minify-img', 'minify-css', 'move-js'])
 
 gulp.task('server', ['connect', 'watch'])
 
